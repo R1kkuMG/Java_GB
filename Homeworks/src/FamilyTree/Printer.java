d359a1a5be50101ffc005183192116ca3a218dd9
Margarita <alis-weasley@mail.ru>
2022-07-17 14:33:12 +0300

    add Printer interface

---
+++ Homeworks/src/FamilyTree/Printer.java
@@ -0,0 +1,9 @@
+package FamilyTree;
+
+import java.util.ArrayList;
+
+public interface Printer {
+    void printPerson(Person person);
+    void printArray(ArrayList<Node> node);
+    void printNode(Node node);
+}
